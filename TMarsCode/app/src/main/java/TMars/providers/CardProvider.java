3abcdd994286832b771cd73c6a343dabcf9805e3
Brody Rasmussen <brodyras@gmail.com>
2021-03-24 15:41:39 -0600

    created static providers of cards and tags (enumerated)

---
+++ TMarsCode/app/src/main/java/TMars/providers/CardProvider.java
@@ -0,0 +1,9 @@
+package TMars.providers;
+
+import java.util.List;
+
+import TMars.model.Card;
+
+public class CardProvider {
+    private static Card[] cards = {};
+}
---
+++ TMarsCode/app/src/main/java/TMars/providers/TagProvider.java
@@ -0,0 +1,18 @@
+package TMars.providers;
+
+public class TagProvider {
+    public enum Tags {
+        Building,
+        Space,
+        Science,
+        Plant,
+        Microbe,
+        Animal,
+        Power,
+        Jovian,
+        Earth,
+        City,
+        Event,
+        Wild,
+    }
+}

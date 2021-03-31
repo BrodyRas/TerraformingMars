a6fe673b46d41386ddf703a8d67faf5a51bb111a
Brody Rasmussen <brodyras@gmail.com>
2021-03-31 10:19:39 -0600

    created corperations.json, inputed the first couple corps

--- TMarsCode/app/src/main/java/TMars/model/Player.java
+++ TMarsCode/app/src/main/java/TMars/model/Player.java
@@ -4,16 +4,26 @@ package TMars.model;
  * Container object, holds anything that needs to be saved between sessions
  */
 public class Player implements java.io.Serializable {
+    private static Player instance;
+
+    private Player(){ }
+
+    public static Player getInstance(){
+        if(instance == null){
+            instance = new Player();
+        }
+        return instance;
+    }
+
     public Tableau tableau;
     //public Hand mHand;
-    //boolean settings //We'll have boolean flags for the settings
+    //boolean settings //We'll have boolean flags for the setting
 
-    public Player(Corporation corp)
+    public void init(Corporation corp)
     {
         tableau = new Tableau(corp);
     }
 
-    public Player(){}
 
     public Tableau getTableau() {
         return tableau;

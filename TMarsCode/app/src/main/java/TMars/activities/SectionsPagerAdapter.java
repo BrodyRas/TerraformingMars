595db57aca5b158c79fe73ccd3010df4178b2b77
Nate Fastar <fastar3@gmail.com>
2021-04-12 18:12:32 -0600

    Help Fragment
    Added the help fragment

--- TMarsCode/app/src/main/java/TMars/activities/SectionsPagerAdapter.java
+++ TMarsCode/app/src/main/java/TMars/activities/SectionsPagerAdapter.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import TMars.fragments.HelpFragment;
 import TMars.fragments.PlaceholderFragment;
 import TMars.fragments.TableauFragment;
 import TMars.fragments.TagFragment;
@@ -23,9 +24,10 @@ class SectionsPagerAdapter extends FragmentStatePagerAdapter {
 
     private static final int TABLEAU_FRAGMENT_POSITION = 0;
     private static final int TAG_FRAGMENT_POSITION = 1;
+    private static final int HELP_FRAGMENT_POSITION = 3;
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tableauTabTitle, R.string.tagTabTitle, R.string.cardTabTitle, R.string.actionTabTitle};
+    private static final int[] TAB_TITLES = new int[]{R.string.tableauTabTitle, R.string.tagTabTitle, R.string.cardTabTitle, R.string.helpTabTitle};
     private Fragment[] tabs = new Fragment[]{null,null,null,null};
     private final Context mContext;
     private final Player player;
@@ -51,6 +53,10 @@ class SectionsPagerAdapter extends FragmentStatePagerAdapter {
         {
             tabs[TAG_FRAGMENT_POSITION] = TagFragment.newInstance(player);
             return tabs[TAG_FRAGMENT_POSITION];
+        } else if (position == HELP_FRAGMENT_POSITION)
+        {
+            tabs[HELP_FRAGMENT_POSITION] = HelpFragment.newInstance();
+            return tabs[HELP_FRAGMENT_POSITION];
         }
         else
         {
---
+++ TMarsCode/app/src/main/java/TMars/fragments/HelpFragment.java
@@ -0,0 +1,29 @@
+package TMars.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import edu.byu.cs.tweeter.R;
+
+public class HelpFragment extends Fragment {
+
+    public static HelpFragment newInstance() {
+        HelpFragment fragment = new HelpFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+
+
+        return view;
+    }
+}
